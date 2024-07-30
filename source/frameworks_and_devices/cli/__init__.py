0512dadbea3f1ec93e54003f2c2c83ae9541e607
Marco Carmona <marcocarmonadev@outlook.com>
2024-07-30 13:38:22 -0600

    Update __init__ files
    Add new commands and modify functions

--- source/frameworks_and_devices/cli/__init__.py
+++ source/frameworks_and_devices/cli/__init__.py
@@ -1,33 +1,81 @@
-from enum import Enum
+import datetime as dt
+from typing import Annotated
+from uuid import UUID
 
 import typer
+
+from source.entities import entry
+from source.frameworks_and_devices import database
+from source.interface_adapters import controllers
 
 app = typer.Typer()
 
 
-class Environment(Enum):
-    DEVELOPMENT = "development"
-    PRODUCTION = "production"
-
-
 @app.callback()
 def callback(
-    environment: Environment = typer.Option(
-        default=Environment.PRODUCTION,
-        envvar="ENVIRONMENT",
-    ),
+    debug: Annotated[
+        bool,
+        typer.Option(
+            "--debug",
+            envvar="DEBUG",
+            hidden=True,
+        ),
+    ] = False,
 ):
-    print(environment)
+    database.set_file_path(debug)
+    database.engine.set_echo(debug)
 
 
 @app.command()
 def init():
-    ...
-    # CURRENT_DIRECTORY_PATH = Path()
-    # PYPROJECT_TOML_FILE_PATH = CURRENT_DIRECTORY_PATH / "pyproject.toml"
-    # if PYPROJECT_TOML_FILE_PATH.exists():
-    #     config.init(
-    #         config_directory_path=CURRENT_DIRECTORY_PATH,
-    #     )
-    # else:
-    #     config.init()
+    try:
+        database.init()
+    except FileExistsError as exc:
+        print(exc)
+        raise typer.Abort()
+
+
+@app.command()
+def create(
+    concept: Annotated[
+        str,
+        typer.Option(
+            prompt=True,
+        ),
+    ],
+    amount: Annotated[
+        float,
+        typer.Option(
+            prompt=True,
+        ),
+    ],
+    due_date: Annotated[
+        dt.datetime,
+        typer.Option(
+            prompt=True,
+        ),
+    ],
+    status: Annotated[
+        entry.Status,
+        typer.Option(
+            prompt=True,
+        ),
+    ],
+):
+    controllers.entry.create()
+
+
+@app.command()
+def read(): ...
+
+
+@app.command()
+def update(
+    uuid: UUID,
+): ...
+
+
+@app.command()
+def delete(
+    uuid: UUID,
+): ...
--- source/frameworks_and_devices/database/__init__.py
+++ source/frameworks_and_devices/database/__init__.py
@@ -1,9 +1,30 @@
-from . import cursor
+from pathlib import Path
 
-FILE_NAME = "data.db"
+from source.entities import Base
+
+from . import engine, session
+
+FILE_NAME = "entries-cli.db"
+DEFAULT_FILE_PATH = Path.home() / f".local/share/{FILE_NAME}"
+
+file_path = DEFAULT_FILE_PATH
 
 
-def init(): ...
+def set_file_path(
+    debug: bool,
+) -> None:
+    if debug:
+        global file_path
+        file_path = Path() / "entries-cli.db"
 
 
-__all__ = ["cursor"]
+def init():
+    if file_path.exists():
+        raise FileExistsError(f"{file_path} already exists!")
+    file_path.touch()
+    Base.metadata.create_all(
+        bind=engine.create(file_path),
+    )
+
+
+__all__ = ["engine", "session"]
