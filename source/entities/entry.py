b90f85a03a33a9631f5be9b0f81ce1652a25efff
Marco Carmona <marcocarmonadev@outlook.com>
2024-07-30 13:35:59 -0600

    Create entry.py
    Add Status Enum and Model

--- source/entities/entry.py
+++ source/entities/entry.py
@@ -1,5 +1,5 @@
 import datetime as dt
-from enum import StrEnum
+from enum import Enum
 from uuid import UUID, uuid4
 
 from sqlalchemy.orm import Mapped, mapped_column
@@ -7,7 +7,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 from . import Base
 
 
-class Status(StrEnum):
+class Status(Enum):
     PENDING = "pending"
     COMPLETED = "completed"
     CLOSED = "closed"
@@ -21,18 +21,15 @@ class Model(Base):
     )
     concept: Mapped[str]
     amount: Mapped[float]
+
+    due_date: Mapped[dt.date]
+    status: Mapped[Status]
+    repeat_forever: Mapped[bool]
+    repeated: Mapped[bool | None]
+    repeat_interval: Mapped[int | None]
     creation_date: Mapped[dt.datetime] = mapped_column(
         default=dt.datetime.now,
     )
-    due_date: Mapped[dt.date]
     uuid: Mapped[UUID] = mapped_column(
         default=uuid4,
-    )
-    status: Mapped[Status]
-    repeat_forever: Mapped[bool]
-    repeated: Mapped[bool | None] = mapped_column(
-        default=None,
-    )
-    repeat_interval: Mapped[int | None] = mapped_column(
-        default=None,
     )
